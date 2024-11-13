f147f8016de72b348667388c5db4f15677fa1cc5
NadiaAhmed15 <nadiaahmedoo15@gmail.com>
2024-11-13 12:15:22 +0300

    Add files via upload

---
+++ src/CardList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from './Card';
+import './CardList.css';
+
+const CardList = ({ cards }) => {
+  return (
+    <div className="card-list">
+      {cards.map((card, index) => (
+        <Card
+          key={index}
+          image={card.image}
+          title={card.title}
+          description={card.description}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default CardList;
